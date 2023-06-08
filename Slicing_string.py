0d02a9027a9dfb0ae676d5d0c5a77666d4f28ff6
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-08 08:26:39 +0600

    Add files via upload

---
+++ Slicing_string.py
@@ -0,0 +1,22 @@
+#Slicing: the start index and the end index, separated by a colon,
+# to return a part of the string.
+
+#slicing: specific index to index
+b = "Hello, World!"
+print(b[2:5])
+
+#slicing from the start
+b = "Hello, World!"
+print(b[:5])
+
+#slicing to the end
+b = "Hello, World!"
+print(b[2:])
+
+# negative slicing
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+
+
