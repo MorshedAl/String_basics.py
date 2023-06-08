47de37ae80be719259dc61acba5b4cd616fd8e1d
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-08 08:44:47 +0600

    Add files via upload

---
+++ Modify_string.py
@@ -0,0 +1,27 @@
+
+#upper()
+a = "Hello, World!"
+print(a.upper())
+
+#lower()
+a = "Hello, World!"
+print(a.lower())
+
+#The strip() method removes any whitespace from the beginning or the end:
+a = " Hello,     World!   Bangladesh   "
+print(a.strip())
+
+#The replace() method replaces a string with another string:
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+#The split() method returns a list 
+#The split() method splits the string into substrings if it finds instances of the separator:
+a = "Hello, World!, Bangladesh "
+b = a.split(",")
+print(b)
+
+
+
+
